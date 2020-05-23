be28f91c15bf3d1fd10ea9337ee7f23dca529667
nicolassciarrotta <62045263+nicolassciarrotta@users.noreply.github.com>
2020-05-23 16:01:04 -0300

    RMI without Rabbit

--- src/main/java/sobelRMI/Server.java
+++ src/main/java/sobelRMI/Server.java
@@ -1,25 +1,15 @@
 package sobelRMI;
 
-import java.io.Serializable;
+
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Server implements Serializable{
+public class Server{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+
 	private static int port = 9000;
-	
-	private int generatePort(int endPort) {
-		if (endPort>9) {
-			return (9020 + endPort);
-		}else
-			return(9010 + endPort);
-	}
 	
 	public static void main(String[] args) {
 		
